ba0bf253ec0c6570eeadbe92eaded6be3305dc84
wabliplatform <93119230+wabliplatform@users.noreply.github.com>
2021-12-22 21:18:49 +0200

    My commit message

--- javascript/homePage.js
+++ javascript/homePage.js
@@ -3,14 +3,14 @@ let apiUserApi = new TempApi.UserApi();import TempApi from '../src/index';window
     if(subDataElements.length > i)
       {
         try { 
-        const subDataElements = subDataElements[i].querySelector("[annotationname = 'userName']");
-        if(subDataElements !== null){
-          subDataElements.textContent = data[data.length -i -1].userName;
+        const htmlElement = subDataElements[i].querySelector("[annotationname = 'userName']");
+        if(htmlElement !== null){
+          htmlElement.textContent = data[data.length -i -1].userName;
         }
        } catch (e) { console.log(e) };try { 
-        const subDataElements = subDataElements[i].querySelector("[annotationname = 'userEmail']");
-        if(subDataElements !== null){
-          subDataElements.textContent = data[data.length -i -1].userEmail;
+        const htmlElement = subDataElements[i].querySelector("[annotationname = 'userEmail']");
+        if(htmlElement !== null){
+          htmlElement.textContent = data[data.length -i -1].userEmail;
         }
        } catch (e) { console.log(e) };
         subDataElements[i].addEventListener('click',() => {{ location.href= '/ShowOneElement/'+data[data.length -i -1]._id+'';}} )
